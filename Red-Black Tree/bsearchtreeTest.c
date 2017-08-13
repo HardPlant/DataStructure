427dfbb189145a7c7e58d7a1c95bdaef96c0a601
HardPlant <abc7988se@naver.com>
2017-08-13 11:47:37 +0900

    tree malloc test

--- Red-Black Tree/bsearchtreeTest.c
+++ Red-Black Tree/bsearchtreeTest.c
@@ -17,13 +17,33 @@ int makeNodeTest(){
     int* testdata = (int*)Node->data;
     _assert((testdata[0] == 1));
     _assert(testdata[1] == 0);
+
+    int* mallocdata = calloc(4, sizeof(int));
+    int mallocdatasize = 4*sizeof(int);
+    mallocdata[0] = 1;
+    mallocdata[1] = 1;
+    mallocdata[2] = 1;
+    mallocdata[3] = 1;
     free(Node);
+    Node = makeNode(0, mallocdata, sizeof(mallocdatasize));
+    _assert(Node);
+    _assert(Node->data);
+    testdata = (int*)Node->data;
+    _assert(testdata[0] == 1);
+    _assert(testdata[1] == 1);
+    _assert(testdata[2] == 1);
+    _assert(testdata[3] == 1);
+    free(testdata);
+    free(Node);
+    
     return 0;
 }
 int makeTreeTest(){
     BTREE Tree = makeTree();
+
     _assert(Tree);
     _assert(BTREE_insert(Tree, 1));
+
 
     free(Tree);
     return 0;
