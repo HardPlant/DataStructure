645568f38dcf751e29942626bc99ee70e7f7f6aa
HardPlant <abc7988se@naver.com>
2017-08-13 10:48:01 +0900

    test

---
+++ Red-Black Tree/bsearchtreeTest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "testframework.h"
+#include "bsearchtree.h"
+
+/*
+int USL_FirstSearch_01()
+{
+    _assert(TRUE)
+	return 0;
+}
+int all_tests() {
+	_verify(BST_FirstSearch_01);
+	return 0;
+}
+*/
+int compileTest(){
+    _assert(TRUE);
+    return 0;
+}
+int all_tests() {
+	_verify(compileTest);
+	return 0;
+}
+
+int main(){
+    testmain();
+}
---
+++ Red-Black Tree/testframework.h
@@ -0,0 +1,27 @@
+#define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
+#define _assert(test) do { if ((!test)) {FAIL(); return 1; } } while (0)
+#define _verify(test) do {int r = test(); tests_run++; if (r) return r; } while(0)
+#define square(n) (n)*(n)
+
+#include <stdio.h>
+
+int tests_run = 0;
+/*
+int USL_FirstSearch_01()
+{
+    _assert(TRUE)
+	return 0;
+}
+int all_tests() {
+	_verify(BST_FirstSearch_01);
+	return 0;
+}
+*/
+int testmain() {
+	int result = all_tests();
+	if (result == 0)
+		printf("PASSED\n");
+	printf("Tests run : %d\n", tests_run);
+
+	return result != 0;
+}
